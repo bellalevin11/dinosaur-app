8ee234fef923e4eee5935e17fad4f79691ec841d
bellalevin11 <bella.l@wustl.edu>
2026-06-12 09:41:58 -0400

    database update

--- dino-project/server/db.js
+++ dino-project/server/db.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.USER,
-  host: "localhost",
-  database: "dino_db",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
